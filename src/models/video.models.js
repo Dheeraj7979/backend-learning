92b30539d29b10ffd116299c36330ac0602abc31
Dheeraj7979 <dheerajku357@gmail.com>
2026-03-23 16:58:25 +0530

    Add video model schema for MongoDB

---
+++ src/models/video.models.js
@@ -0,0 +1,39 @@
+import mongoose,{Schema} from 'mongoose'
+
+const videoSchema =Schema({
+     videoFile:{
+          type:String,
+          required:true,
+     },
+     thumbnail:{
+          type:String,
+          required:true,
+     },
+     owner:{
+          type:Schema.Types.ObjectId,
+          ref:"User",
+          required:true,
+     },
+     title:{
+          type:String,
+          required:true,
+     },
+     description:{
+          type:String,
+          required:true,
+     },
+     duration:{
+          type:String,
+     },
+     views:{
+          type:Number,
+          default:0
+     },
+     isPublished:{
+          type:Boolean,
+          default:true,
+     }
+
+},{timestamps:true})
+
+export const Video = mongoose.model("Video",videoSchema)
